977df101d37eec30475aa0184b9c7fe1f725da3a
darmanto <darmanto@cititex.com>
2019-04-29 14:43:09 +0700

    update slide

--- static/js/12.355cb47f.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{1240:function(e,n,a){"use strict";a.r(n);var t=a(0),c=a.n(t),r=a(454);n.default=function(){return c.a.createElement("div",{className:"loader-container"}," ",c.a.createElement(r.a,null)," ")}}}]);
-//# sourceMappingURL=12.355cb47f.chunk.js.map
